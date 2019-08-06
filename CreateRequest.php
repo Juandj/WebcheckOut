87c901299ef5d98bab210ee0a773ff9578ca6e58
Juan David Jiménez  Arboleda <juan.jimenez@egm.local>
2019-08-06 17:51:39 -0500

    Test

---
+++ CreateRequest.php
@@ -0,0 +1,74 @@
+<?php
+
+include('Autentication.php');
+
+class CreateRequest 
+{
+    public function session($ctype, $amount, $name, $surname, $email, $phone, $mobile)
+    {
+        $request = new stdClass();
+        $new = new Autentication();
+        $request->auth = $new ->auth();
+        $request->buyer = $new ->buyerSet();
+        $request->payment = $new -> amountSet("90.36");
+        $request->expiration = "2019-08-03T15:43:05-05:00";
+        $request->returnUrl = "http://localhost/webcheckout/";
+        $request->ipAddress = "192.162.12.34";
+        $request->userAgent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64)";
+
+        $denco= json_encode($request);
+        // return $denco;
+        $url = 'https://test.placetopay.ec/redirection/api/session';
+        //Se inicia. el objeto CUrl
+        $ch = curl_init($url);
+        //creamos el json a partir del arreglo
+        //$jsonDataEncoded = json_encode($request);
+        //Indicamos que nuestra petición sera Post
+        curl_setopt($ch, CURLOPT_POST, 1);
+        //para que la peticion no imprima el resultado como un echo comun, y podamos manipularlo
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //Adjuntamos el json a nuestra petición  $jsonDataEncoded
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $denco);
+          //Agregar los encabezados del contenido
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'User-Agent: cUrl Testing'));
+        //Ejecutamos la petición
+        $result = curl_exec($ch);
+        //$R = ((string) json_encode($request));
+        return  $result;
+    }
+
+    public function getRequesInformation()
+    {
+        $request = new stdClass();
+        $new = new Autentication();
+        $request->auth = $new ->auth();
+
+        $denco= json_encode($request);
+        // return $denco;
+        $url = 'https://test.placetopay.ec/redirection/api/session/'.'115861';
+        //Se inicia. el objeto CUrl
+        $ch = curl_init($url);
+        //creamos el json a partir del arreglo
+        //$jsonDataEncoded = json_encode($request);
+        //Indicamos que nuestra petición sera Post
+        curl_setopt($ch, CURLOPT_POST, 1);
+        //para que la peticion no imprima el resultado como un echo comun, y podamos manipularlo
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //Adjuntamos el json a nuestra petición  $jsonDataEncoded
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $denco);
+          //Agregar los encabezados del contenido
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'User-Agent: cUrl Testing'));
+        //Ejecutamos la petición
+        $result = curl_exec($ch);
+        //$R = ((string) json_encode($request));
+        return  $result;
+    }
+}
+
+$new = new CreateRequest();
+//Cambiar por el metodo que se desea invocar
+$resultado = $new ->getRequesInformation();
+$denco = json_encode($resultado);
+echo $denco;
+
+?>
---
+++ Formulario.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Pagos</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="well well-sm">
+                    <form class="form-horizontal" method="post" action="Validar_post.php">  
+                        <fieldset>
+                            <legend class="text-center header">Pagos</legend>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <select id="ctype" name="ctype" class="form-control" title="Currency Type">
+                                       <optgroup label="Currency type">
+                                       <option></option>
+                                        <option>Colombian Peso</option>
+                                        <option>US Dollar</option>
+                                        </optgroup>
+                                     </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="amount" name="amount" type="text" placeholder="Amount" class="form-control">
+                                </div>
+                            </div>
+                           
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="name" name="name" type="text" placeholder="Name" class="form-control">
+                                </div>
+                            </div>
+                           
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="surname" name="surname" type="text" placeholder="Sur name" class="form-control">
+                                </div>
+                            </div>
+                           
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="email" name="email" type="text" placeholder="Correo electronico" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="document" name="phone" type="document" placeholder="Document" class="form-control">
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="mobile" name="mobile" type="document" placeholder="Mobile" class="form-control">
+                                </div>
+                            </div>
+
+
+                            <div class="form-group">
+                                <div class="col-md-12 text-center">
+                                    <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
